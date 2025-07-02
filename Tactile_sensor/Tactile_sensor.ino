7aaa7f306748e207faaaf88a3303155c565565c9
Protteo <matteo611.prov@gmail.com>
2025-07-02 12:25:52 +0200

    Test avec deux capteurs

--- Tactile_sensor/Tactile_sensor.ino
+++ Tactile_sensor/Tactile_sensor.ino
@@ -1,6 +1,6 @@
 //Le code est bon
 const int capteurPin = A17; // Broche analogique utilisée
-//const int capteurPin2 = A1;
+const int capteurPin2 = A16;
 const float VREF = 3.3;    // Tension de référence analogique (adapter à ton Teensy)
 const int resolution = 1024; // Résolution ADC (10 bits sur Teensy 3.2 ; 12 bits possible)
 
@@ -11,10 +11,10 @@ void setup() {
 
 void loop() {
   int valeurBrute = analogRead(capteurPin);
-//  int valeurBrute2 = analogRead(capteurPin2);
+  int valeurBrute2 = analogRead(capteurPin2);
 
   float tension = (valeurBrute / (float)(resolution - 1)) * VREF;
-//  float tension2 = (valeurBrute2 / (float)(resolution - 1)) * VREF;
+  float tension2 = (valeurBrute2 / (float)(resolution - 1)) * VREF;
 
   Serial.print("Valeur brute : ");
   Serial.print(valeurBrute);
@@ -22,11 +22,11 @@ void loop() {
   Serial.print(tension, 3);
   Serial.println(" V");
 
-//  Serial.print("Valeur brute 2 : ");
-//  Serial.print(valeurBrute2);
-//  Serial.print("\tTension : ");
-//  Serial.print(tension2, 3);
-//  Serial.println(" V");
+  Serial.print("Valeur brute 2 : ");
+  Serial.print(valeurBrute2);
+  Serial.print("\tTension : ");
+  Serial.print(tension2, 3);
+  Serial.println(" V");
 
   delay(200);
 }
