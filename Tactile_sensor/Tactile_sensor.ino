cc735ac42a532fa855df1b1664eb124ecd878406
Protteo <matteo611.prov@gmail.com>
2025-07-03 12:33:06 +0200

    Update Tactile_sensor.ino

--- Tactile_sensor/Tactile_sensor.ino
+++ Tactile_sensor/Tactile_sensor.ino
@@ -1,6 +1,8 @@
 //Le code est bon
 const int capteurPin = A17; // Broche analogique utilisée
 const int capteurPin2 = A16;
+//const int capteurPin3 = A24;
+
 const float VREF = 3.3;    // Tension de référence analogique (adapter à ton Teensy)
 const int resolution = 1024; // Résolution ADC (10 bits sur Teensy 3.2 ; 12 bits possible)
 
@@ -12,6 +14,8 @@ void setup() {
 void loop() {
   float valeurBrute = analogRead(capteurPin);
   float valeurBrute2 = analogRead(capteurPin2);
+//  float valeurBrute3 = analogRead(capteurPin3);
+
 
 //  float tension = (valeurBrute / (float)(resolution - 1)) * VREF;
 //  float tension2 = (valeurBrute2 / (float)(resolution - 1)) * VREF;
@@ -30,13 +34,24 @@ void loop() {
 //  Serial.print(tension2, 3);
 //  Serial.println(" V");
 
+//  Serial.print("Valeur brute 3 : ") ;
+//  Serial.print(valeurBrute3);
+//  Serial.print("\t");
+
   float rapport = valeurBrute/valeurBrute2;
   float somme = valeurBrute + valeurBrute2;
-  Serial.print("Rapport des deux données brutes :");
+  Serial.print("Rapport :");
   Serial.print(rapport);
   Serial.print("\t");
-  Serial.print("Somme des deux valeurs brutes :");
-  Serial.println(somme);
-
+//  Serial.print("Somme des deux valeurs brutes :");
+//  Serial.println(somme);
   delay(200);
-}
+  if (1010 >  valeurBrute2)
+  {
+    Serial.println("Tu le tiens en poignard");
+  }
+  else
+  {
+    Serial.println("Tu le tiens en standard");
+  }
+  }
