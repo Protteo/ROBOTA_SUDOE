a5d8a997040c9b162be18eb272cd5b43fa9df3d9
Protteo <matteo611.prov@gmail.com>
2025-07-06 14:07:43 +0200

    Ajout du code
    Permet de plot le centre des forces avec python

---
+++ Plot_pos/Plot_pos.py
@@ -0,0 +1,34 @@
+#%%--------------------Importer les library------------------------------------
+import serial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#%%--------------------Ouvre le port série-------------------------------------
+ser = serial.Serial("COM3",9600)
+
+#%%--------------------Preparer la figure--------------------------------------
+fig, ax = plt.subplots()
+point, =ax.plot([], [], 'ro')
+ax.set_xlim(-1,1)
+ax.set_ylim(-1,1)
+ax.set_xlabel("Latéral")
+ax.set_ylabel("Vertical")
+ax.grid(True)
+
+def update(frame):
+    line = ser.readline().decode().strip()
+    try:
+        x_str, y_str = line.split(",")
+        x=float(x_str)
+        y=float(y_str)
+        point.set_data(x, y)
+    except:
+        pass
+    return point,
+
+ani = animation.FuncAnimation(fig, update, blit=True, interval=50)
+plt.show()
+
+
+
+
--- Tactile_sensor/Tactile_sensor.ino
+++ Tactile_sensor/Tactile_sensor.ino
@@ -28,7 +28,23 @@ void loop() {
   float haut = capteur1 +capteur2;
   float bas= capteur3+ capteur4;
   float Tot= haut + bas ;
+
+  float x1 = - valeurBrute * sqrt(2)/2;
+  float x2 = valeurBrute2 * sqrt(2)/2;
+  float x3 = - valeurBrute3* sqrt(2)/2;
+  float x4 = valeurBrute4 * sqrt(2)/2;
+  float x_tot = x1 + x2 + x3 + x4;
+  float x_sum = abs(x1) + abs(x2) + abs(x3) + abs(x4); 
+  float x=x_tot/x_sum;
   
+
+  float y1 = valeurBrute * sqrt(2)/2;
+  float y2 = valeurBrute2 * sqrt(2)/2;
+  float y3 = - valeurBrute3* sqrt(2)/2;
+  float y4 = - valeurBrute4 * sqrt(2)/2;
+  float y_tot = y1 + y2 + y3 + y4;
+  float y_sum = abs(y1) + abs(y2) + abs(y3) + abs(y4);
+  float y = y_tot/y_sum;
   
 
 //  float tension = (valeurBrute / (float)(resolution - 1)) * VREF;
@@ -71,6 +87,12 @@ void loop() {
   Serial.print("Cpt4 : ");
   Serial.println(capteur4);
 
+  Serial.print("(");
+  Serial.print(x);
+  Serial.print(",")
+  Serial.print(y);
+  Serial.println(")");
+
 //  Serial.print("\t");
 //  Serial.print("\tTension : ");
 //  Serial.print(tension, 3);
@@ -79,8 +101,8 @@ void loop() {
 //  float avant = (valeurBrute+valeurBrute2)/2;
 //  float arriere = (valeurBrute4+valeurBrute3)/2;
 
-  float somme_standard = capteur1 + capteur2;
-  float somme_poignard = capteur2 + capteur3;
+//  float somme_standard = capteur1 + capteur2;
+//  float somme_poignard = capteur2 + capteur3;
 //  Serial.print("Avant :");
 //  Serial.print(avant);
 //  Serial.print("\t");
@@ -88,30 +110,31 @@ void loop() {
 //  Serial.print("Arrière :");
 //  Serial.print(arriere);
 //  Serial.print("\t");
-  Serial.print("Somme standard :");
-  Serial.print(somme_standard);
-  Serial.print("\t");
-  Serial.print("Somme poignard :");
-  Serial.println(somme_poignard);
-  Serial.print("droite : ");
-  Serial.println(droite);
-  Serial.print("gauche : ");
-  Serial.println(gauche);
-  Serial.print("haut : ");
-  Serial.println(haut);
-  Serial.print("bas : ");
-  Serial.println(bas);
-  Serial.print("Tot : ");
-  Serial.println(Tot);
+//  Serial.print("Somme standard :");
+//  Serial.print(somme_standard);
+//  Serial.print("\t");
+//  Serial.print("Somme poignard :");
+//  Serial.println(somme_poignard);
+//  Serial.print("droite : ");
+//  Serial.println(droite);
+//  Serial.print("gauche : ");
+//  Serial.println(gauche);
+//  Serial.print("haut : ");
+//  Serial.println(haut);
+//  Serial.print("bas : ");
+//  Serial.println(bas);
+//  Serial.print("Tot : ");
+//  Serial.println(Tot);
 
   
-  delay(200);
-  if (somme_standard > somme_poignard or abs(somme_standard-somme_poignard)<300)
-  {
-    Serial.println("Tu le tiens en poignard");
-  }
-  else
-  {
-    Serial.println("Tu le tiens en standard");
-  }
+//  delay(200);
+//  if (somme_standard > somme_poignard or abs(somme_standard-somme_poignard)<300)
+//  {
+//    Serial.println("Tu le tiens en poignard");
+//  }
+//  else
+//  {
+//    Serial.println("Tu le tiens en standard");
+//  }
+  delay(50);
   }
