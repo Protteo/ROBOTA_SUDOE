f4762b29407626fb392833084fdca0a7f0a192bb
Protteo <matteo611.prov@gmail.com>
2025-07-10 13:09:41 +0200

    Code arduino épuré

--- Tactile_sensor/Tactile_sensor.ino
+++ Tactile_sensor/Tactile_sensor.ino
@@ -30,12 +30,6 @@ void loop() {
   float capteur5 = -(100.0/1020)*valeurBrute5+100;
   float capteur6 = (-100.0/1020)*valeurBrute6+100;
  
-//  float droite = capteur2 + capteur4;
-//  float gauche = capteur1 + capteur3;
-//  float haut = capteur1 +capteur2;
-//  float bas= capteur3+ capteur4;
-//  float Tot= haut + bas ;
-
 //--------Pour graphique----------------------------------
 //  float x1 =  valeurBrute1 * sqrt(2)/2;
 //  float x2 = -valeurBrute2 * sqrt(2)/2;
@@ -56,7 +50,7 @@ void loop() {
 //  float y_sum = abs(y1) + abs(y2) + abs(y3) + abs(y4);
 //  float y = y_tot/y_sum;
   
-//--------------------------------------------------------------------------------
+//--------------------------Valeurs brutes-----------------------------------
 //  float tension = (valeurBrute / (float)(resolution - 1)) * VREF;
 //  float tension2 = (valeurBrute2 / (float)(resolution - 1)) * VREF;
 //  Serial.print("Val brute 1 : ");
@@ -75,7 +69,7 @@ void loop() {
 //  Serial.print(valeurBrute4);
 //  Serial.print("\t");
 
-
+//-------------------------------Valeurs des capteurs après ajustement------------------------
 //  Serial.print("Cpt1 : ");
   Serial.print(capteur1);
   Serial.print(",");
@@ -111,48 +105,5 @@ void loop() {
 //  Serial.println(")");
 //---------------------------------------------------------------------------
 
-//  Serial.print("\t");
-//  Serial.print("\tTension : ");
-//  Serial.print(tension, 3);
-//  Serial.println(" V");
-
-//  float avant = (valeurBrute+valeurBrute2)/2;
-//  float arriere = (valeurBrute4+valeurBrute3)/2;
-
-//  float somme_standard = capteur1 + capteur2;
-//  float somme_poignard = capteur2 + capteur3;
-//  Serial.print("Avant :");
-//  Serial.print(avant);
-//  Serial.print("\t");
-  
-//  Serial.print("Arrière :");
-//  Serial.print(arriere);
-//  Serial.print("\t");
-//  Serial.print("Somme standard :");
-//  Serial.print(somme_standard);
-//  Serial.print("\t");
-//  Serial.print("Somme poignard :");
-//  Serial.println(somme_poignard);
-//  Serial.print("droite : ");
-//  Serial.println(droite);
-//  Serial.print("gauche : ");
-//  Serial.println(gauche);
-//  Serial.print("haut : ");
-//  Serial.println(haut);
-//  Serial.print("bas : ");
-//  Serial.println(bas);
-//  Serial.print("Tot : ");
-//  Serial.println(Tot);
-
-  
-//  delay(200);
-//  if (somme_standard > somme_poignard or abs(somme_standard-somme_poignard)<300)
-//  {
-//    Serial.println("Tu le tiens en poignard");
-//  }
-//  else
-//  {
-//    Serial.println("Tu le tiens en standard");
-//  }
     delay(10);
   }
